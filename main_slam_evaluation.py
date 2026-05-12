c14ccaa02f37d4fb87ca63853d58a1edc221f94c
adamburhan <adam14011999@gmail.com>
2026-05-12 12:52:33 -0400

    minor fix

--- main_slam_evaluation.py
+++ main_slam_evaluation.py
@@ -17,6 +17,8 @@
 * You should have received a copy of the GNU General Public License
 * along with PYSLAM. If not, see <http://www.gnu.org/licenses/>.
 """
+import pandas    
+import jinja2    
 
 import sys
 import pyslam.config as config
